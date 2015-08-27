86e8fe83dadc801606d31c77dec0e920257439ba
Jason Daly <jason@deefour.me>
2015-08-27 11:20:19 -0400

    synchronous watch setup

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,10 @@
-var gulp   = require('gulp');
-var config = require('../config');
+var gulp        = require('gulp');
+var config      = require('../config');
+var runSequence = require('run-sequence');
 
-gulp.task('watch', [ 'watchify' ], function() {
+gulp.task('watch', function(cb) {
+  runSequence('clean', [ 'watchify', 'sass', 'images', 'extras', 'markup' ], cb);
+
   gulp.watch(config.css.watch,    [ 'sass' ]);
   gulp.watch(config.images.src,   [ 'images' ]);
   gulp.watch(config.markup.watch, [ 'markup' ]);
