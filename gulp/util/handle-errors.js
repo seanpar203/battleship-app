794fc8d7275afafc9cf04ba4c025dd51588ef885
Jason Daly <jason@deefour.me>
2015-08-27 11:20:30 -0400

    prettier logs

--- gulp/util/handle-errors.js
+++ gulp/util/handle-errors.js
@@ -1,14 +1,9 @@
-var notify = require("gulp-notify");
+var gutil = require('gulp-util');
 
 module.exports = function() {
-
   var args = Array.prototype.slice.call(arguments);
 
-  // Send error to notification center with gulp-notify
-  notify.onError({
-    title: "Compile Error",
-    message: "<%= error %>"
-  }).apply(this, args);
+  gutil.log(gutil.colors.red(args[0].message));
 
   // Keep gulp from hanging on this task
   this.emit('end');
