f978afba6c8d682deb646be3180a71c75b5edba8
Jason Daly <jason@deefour.me>
2016-02-01 11:18:15 -0500

    don't use frowned upon 'paths' option in bundle config

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -18,8 +18,7 @@ module.exports = {
       {
         entries:    src + '/assets/javascripts/app.js',
         dest:       dest + '/assets',
-        outputName: 'app.js',
-        paths:      [ src + '/assets/javascripts',  src + '/templates' ]
+        outputName: 'app.js'
       }
     ]
 
