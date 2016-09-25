4c2a8283febfd502ecfba5ff793d1b912578a0a6
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-25 10:46:59 -0400

    Added dotenv require to access dynamic base url.

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -2,5 +2,6 @@ var gulp        = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('default', function(cb) {
+  require('dotenv').config();
   runSequence('clean', [ 'sass', 'images', 'extras', 'html' ], 'live-update', cb);
 });
